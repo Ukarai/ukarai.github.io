f7a77e29a29d5eddc9ea5b53dfea9cac4e8fc7cd
Ukarai <david.house97@yahoo.com>
2020-03-30 20:58:30 +0100

    ,

---
+++ build/precache-manifest.4816ed39bc5816e03e48d42da7524ae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990847bbdecb308fdcc1efe799233641",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e303d645c55ec7a4eef2",
+    "url": "/static/css/main.2e19e7d9.chunk.css"
+  },
+  {
+    "revision": "20e8b8a778d425cfcb4d",
+    "url": "/static/js/2.82c8520e.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.82c8520e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e303d645c55ec7a4eef2",
+    "url": "/static/js/main.19228d0e.chunk.js"
+  },
+  {
+    "revision": "6a160c3e5ac7ff2bdcc9",
+    "url": "/static/js/runtime-main.51e592fc.js"
+  },
+  {
+    "revision": "77443d8a63a04c849419c7788c6cbad3",
+    "url": "/static/media/skills.77443d8a.txt"
+  }
+]);
